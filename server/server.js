bbc3afa81a6b70aea6a8b10fda12b384fde9842f
whimpe <willemhimp@gmail.com>
2020-10-14 20:05:22 +0200

    first commit

---
+++ server/server.js
@@ -0,0 +1,32 @@
+
+const express = require('express');
+const cors = require('cors');
+const mongoose =require('mongoose');
+
+require('dotenv').config();
+
+const app = express();
+const port = process.env.PORT || 3001;
+
+
+const uri = process.env.ATLAS_URI;
+mongoose.connect(uri, { useNewUrlParser: true, useCreateIndex: true }
+);
+const connection = mongoose.connection;
+connection.once('open', () => {console.log("MongoDB database connection established successfully");
+    })
+
+
+app.use(cors());
+app.use(express.json());
+
+
+const exerciseRouter = require('./routes/Exercise');
+const userRouter = require('./routes/User');
+
+app.use('/exercises', exerciseRouter);
+app.use('/users',userRouter);
+
+app.listen(port, () => {
+    console.log(`Server is running on port: ${port}`);
+});
--- src/App.js
+++ src/App.js
@@ -1,24 +1,30 @@
 import React from 'react';
-import logo from './logo.svg';
+import "bootstrap/dist/css/bootstrap.min.css";
+import { BrowserRouter as Router, Route } from 'react-router-dom'
+
 import './App.css';
+
+import Navbar from "./components/Navbar";
+import ExercisesList from "./components/ExercisesList";
+import EditExercise from "./components/EditExercise";
+import CreateExercise from "./components/CreateExercise";
+import CreateUser from "./components/CreateUser";
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
+      <Router>
+        <Navbar />
+        <br />
+        <Route path="/" exact component={ExercisesList} />
+        <Route path="/edit/:id" exact component={EditExercise} />
+        <Route path="/create" exact component={CreateExercise} />
+        <Route path="/user" exact component={CreateUser} />
+      </Router>
+     
           Learn React
-        </a>
-      </header>
+  
     </div>
   );
 }
